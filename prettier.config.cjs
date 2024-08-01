bc0c8e171f425a09b0f26cdf94153d6c0eac47a3
Jacob Thomas <jacobthomas0898@gmail.com>
2024-08-01 16:25:50 -0400

    initial commit

---
+++ prettier.config.cjs
@@ -0,0 +1,22 @@
+const prettierConfig = {
+  plugins: [require.resolve("prettier-plugin-tailwindcss")],
+  printWidth: 100,
+  trailingComma: "es5",
+  tabWidth: 2,
+  semi: true,
+  singleQuote: false,
+  arrowParens: "always",
+  useTabs: false,
+  overrides: [
+    {
+      files: ".editorconfig",
+      options: { parser: "yaml" },
+    },
+    {
+      files: "LICENSE",
+      options: { parser: "markdown" },
+    },
+  ],
+};
+
+module.exports = prettierConfig;
