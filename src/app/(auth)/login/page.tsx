f131ed6f88161c39f597925396bc57e10274d5c8
Jacob Thomas <jacobthomas0898@gmail.com>
2024-08-01 22:55:55 -0400

    create signup and login pages

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,28 @@
+import loginImage from "@/assets/login-image.jpg";
+import type { Metadata } from "next";
+import Image from "next/image";
+import Link from "next/link";
+import LoginForm from "./LoginForm";
+
+export const metadata: Metadata = {
+  title: "Login",
+};
+
+export default function Page() {
+  return (
+    <main className="flex h-screen items-center justify-center p-5">
+      <div className="bg-card flex h-full max-h-[40rem] w-full max-w-[64rem] overflow-hidden rounded-2xl shadow-2xl">
+        <div className="w-full space-y-10 overflow-y-auto p-10 md:w-1/2">
+          <h1 className="text-center text-3xl font-bold">Login to tweedy</h1>
+          <div className="space-y-5">
+            <LoginForm />
+            <Link href="/signup" className="block text-center hover:underline">
+              Don&apos;t have an account? Sign Up
+            </Link>
+          </div>
+        </div>
+        <Image src={loginImage} alt="" className="hidden w-1/2 object-cover md:block" />
+      </div>
+    </main>
+  );
+}
