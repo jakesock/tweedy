f3ecad4ebe3007f7f66658cde548722ac3c3b362
Jacob Thomas <jacobthomas0898@gmail.com>
2024-08-06 19:02:13 -0400

    menu bar

---
+++ src/app/(main)/MenuBar.tsx
@@ -0,0 +1,61 @@
+import { Button } from "@/components/ui/button";
+import { BellIcon, BookmarkIcon, HomeIcon, MailIcon } from "lucide-react";
+import Link from "next/link";
+
+interface MenuBarProps {
+  className?: string;
+}
+
+export default function MenuBar({ className }: MenuBarProps) {
+  return (
+    <div className={className}>
+      <Button
+        variant="ghost"
+        className="flex items-center justify-start gap-3"
+        title="Home"
+        asChild
+      >
+        <Link href="/">
+          <HomeIcon />
+          <span className="hidden lg:inline">Home</span>
+        </Link>
+      </Button>
+
+      <Button
+        variant="ghost"
+        className="flex items-center justify-start gap-3"
+        title="Notifications"
+        asChild
+      >
+        <Link href="/notifications">
+          <BellIcon />
+          <span className="hidden lg:inline">Notifications</span>
+        </Link>
+      </Button>
+
+      <Button
+        variant="ghost"
+        className="flex items-center justify-start gap-3"
+        title="Messages"
+        asChild
+      >
+        <Link href="/messages">
+          <MailIcon />
+          <span className="hidden lg:inline">Messages</span>
+        </Link>
+      </Button>
+
+      <Button
+        variant="ghost"
+        className="flex items-center justify-start gap-3"
+        title="Bookmarks"
+        asChild
+      >
+        <Link href="/bookmarks">
+          <BookmarkIcon />
+          <span className="hidden lg:inline">Bookmarks</span>
+        </Link>
+      </Button>
+    </div>
+  );
+}
